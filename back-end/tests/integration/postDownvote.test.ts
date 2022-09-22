1c6e37f6a2b049d0aa69814c01de6baf23f325b4
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-09-22 16:09:17 -0300

    feat: post upvote is done

--- back-end/tests/integration/postDownvote.test.ts
+++ back-end/tests/integration/postDownvote.test.ts
@@ -9,13 +9,13 @@ beforeEach(async()=> {
     await deleteAllData()
 })
 
-describe("Test POST /recommendations/:id/upvote", () => { 
+describe("Test POST /recommendations/:id/downvote", () => { 
     it("Have to answer 200, if the user send the corretly data", async() => { 
         const recommendations = await __createRecommendation();
 
         await server.post("/recommendations").send(recommendations); 
         const { body } = await server.get("/recommendations").send();
-        const { status } = await server.post(`/recommendations/${body[0].id}/upvote`);
+        const { status } = await server.post(`/recommendations/${body[0].id}/downvote`);
 
         expect(status).toBe(200);
         expect(body).toBeInstanceOf(Array);
@@ -23,7 +23,7 @@ describe("Test POST /recommendations/:id/upvote", () => {
     }); 
 
     it("Have to answer 404, if the user send an id that doesn't exist", async() => { 
-        const { status } = await server.post(`/recommendations/1/upvote`);
+        const { status } = await server.post(`/recommendations/1/downvote`);
 
         expect(status).toBe(404);
     }); 
