7e9d3293ff8e3bd196d804aa1d0d428b9a15187d
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-09-22 10:36:44 -0300

    feat: POST 201 integration test is done

---
+++ back-end/tests/factories/recommendationsFactory.ts
@@ -0,0 +1,15 @@
+import { faker }from "@faker-js/faker"
+import dotenv from "dotenv";
+import randomUrlGen from "random-youtube-music-video";
+dotenv.config();
+
+export async function __createRecommendation() { 
+    const songName: string = faker.music.songName();
+    const youtubeLink: string = await randomUrlGen.getRandomMusicVideoUrl();
+    console.log(youtubeLink);
+
+    return { 
+        name: songName, 
+        youtubeLink: youtubeLink
+    }
+}
