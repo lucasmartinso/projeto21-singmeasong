d668a0047fefffcab3271ab264ff6b777bed5364
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-09-24 12:20:29 -0300

    feat: postDownvote is done

--- back-end/tests/unit/postDownvoteService.test.ts
+++ back-end/tests/unit/postDownvoteService.test.ts
@@ -15,45 +15,59 @@ afterEach(() => {
   });
 
 describe("Unit tests about downvote service", () => { 
-    /*it.todo("Have to update -1 at score of an recommendation"), async() => {  
+    it("Have to update -1 at score of an recommendation", async() => {  
 
-        jest.spyOn(recommendationRepository, 'find').mockImplementation((): any => {}); 
-        jest.spyOn(recommendationRepository,'updateScore').mockImplementation((): any => {
+        jest.spyOn(recommendationRepository, 'find').mockImplementation((): any => {
             return {
-                id: 1, 
+                id: 2, 
                 name: "Coldplay - Paradise (Official Video)", 
                 youtubeLink: "https://www.youtube.com/watch?v=1G4isv_Fylg", 
                 score: 0
             }
         }); 
+        jest.spyOn(recommendationRepository,'updateScore').mockImplementation((): any => {
+            return {
+                id: 2, 
+                name: "Coldplay - Paradise (Official Video)", 
+                youtubeLink: "https://www.youtube.com/watch?v=1G4isv_Fylg", 
+                score: -1
+            }
+        }); 
         jest.spyOn(recommendationRepository,'remove').mockImplementation((): any => {}); 
 
-        await recommendationService.downvote(1);
+        await recommendationService.downvote(2);
         
         expect(recommendationRepository.find).toBeCalled(); 
         expect(recommendationRepository.updateScore).toBeCalled();
         expect(recommendationRepository.remove).not.toBeCalled();
-    });*/
+    });
 
-    /*it.todo("Have to delete recommendation if his score is less than -5"), async() => {  
+    it("Have to delete recommendation if his score is less than -5", async() => {  
 
-        jest.spyOn(recommendationRepository, 'findByName').mockImplementation((): any => {
+        jest.spyOn(recommendationRepository, 'find').mockImplementation((): any => {
             return {
-                id: 1, 
-                name: "Coldplay - Yellow (Official Video)", 
-                youtubeLink: "https://www.youtube.com/watch?v=yKNxeF4KMsY", 
+                id: 2, 
+                name: "Coldplay - Paradise (Official Video)", 
+                youtubeLink: "https://www.youtube.com/watch?v=1G4isv_Fylg", 
+                score: -5
+            }
+        }); 
+        jest.spyOn(recommendationRepository,'updateScore').mockImplementation((): any => {
+            return {
+                id: 2, 
+                name: "Coldplay - Paradise (Official Video)", 
+                youtubeLink: "https://www.youtube.com/watch?v=1G4isv_Fylg", 
                 score: -6
             }
-        });  
-        jest.spyOn(recommendationRepository, 'remove').mockImplementation((): any => {});
-        jest.spyOn(recommendationRepository, 'updateScore').mockImplementation((): any => {}); 
-        
-        await recommendationService.downvote(1);
+        }); 
+        jest.spyOn(recommendationRepository,'remove').mockImplementation((): any => {}); 
 
+        await recommendationService.downvote(2);
+        
+        expect(recommendationRepository.find).toBeCalled(); 
+        expect(recommendationRepository.updateScore).toBeCalled();
         expect(recommendationRepository.remove).toBeCalled();
-        expect(recommendationRepository.find).toBeCalled();
-        expect(recommendationRepository.updateScore).not.toBeCalled();
-    });*/
+    });
 
     it("Have to denied permission to update a nonexistent recommendation", async() => {  
 
