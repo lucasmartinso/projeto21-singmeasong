986c3fd9680774c57217781480c0261d9e7400d3
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-09-26 11:14:59 -0300

    feat: check if top and random are at the right url address

--- front-end/cypress/e2e/randomMusic.cy.js
+++ front-end/cypress/e2e/randomMusic.cy.js
@@ -1,11 +1,8 @@
-import randomUrlGen from "random-youtube-music-video";
-import { faker } from "@faker-js/faker"
-
-describe('Test POST random', () => {
-  it('Post /recommendations/random', async() => {
+describe('Test GET random', () => {
+  it('Get /recommendations/random', async() => {
     cy.visit('http://localhost:3000/');
 
     cy.get('#random').click(); 
-    cy.url().should('equal','http://localhost:3000/top');
+    cy.url().should('equal','http://localhost:3000/random');
   })
 })
--- front-end/cypress/e2e/topMusics.cy.js
+++ front-end/cypress/e2e/topMusics.cy.js
@@ -1,8 +1,5 @@
-import randomUrlGen from "random-youtube-music-video";
-import { faker } from "@faker-js/faker"
-
-describe('Test POST random', () => {
-  it('Post /recommendations/random', async() => {
+describe('Test GET top', () => {
+  it('Get /recommendations/top', async() => {
     cy.visit('http://localhost:3000/');
 
     cy.get('#top').click(); 
